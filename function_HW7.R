b54c0d059ea80e82864e4b1d3f39d39e89820289
brittanymaldonado <brittany.maldonado@selu.edu>
2020-11-16 15:54:05 -0600

    recommitting functions and data without extraneous files in package

---
+++ function_HW7.R
@@ -0,0 +1,20 @@
+#' Creating a year column from dates for easy grouping and visualization
+#'
+#' @param data data.frame to use
+#' @return same data.frame but now with a column that just has Year for each entry
+#' @importFrom lubridate mdy
+#' @importFrom assertthat assert_that
+#' @export
+#' @example
+#' yearify(agp_biomass)
+#'
+#'
+
+
+yearify <- function(data){
+  assert_that(is.data.frame(data))
+  data$Date1 <- mdy(data$Date)
+data_yr <- data %>% mutate(Year = year(Date1))
+return(data_yr)
+}
+
